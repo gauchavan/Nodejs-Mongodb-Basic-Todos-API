9c3ba72b324d609ca72c317ed42c0a2bc4285faa
Gauri Chavan <chavan.gau@husky.neu.edu>
2019-01-10 09:46:08 +0530

    Mongoose queries!!

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,26 @@
+const { ObjectID } = require('mongodb');
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+
+var id = '5c357ac2a851e23d13668062';
+
+if(!ObjectID.isValid(id)){
+    console.log('Id is not valid');
+}
+
+// Find all Object query
+Todo.find().then((todos) => {
+    console.log('Todos', todos);
+})
+
+//Find one object
+Todo.findOne({
+    _id: id
+}).then((todo) => {
+    console.log('Todo', todo)
+})
+
+// Find by Id 
+Todo.findById(id).then((todo) => {
+    console.log('Todo by Id', todo)
+})
